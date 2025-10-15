32d0f626cf62c10d338b5b72fd6fb8851bd48bd6
abraham abanto <ab.abanto@duocuc.cl>
2025-10-14 21:18:57 -0300

    Se cambio el nombre del saludo

--- QualifyGym_Grupo13/app/src/main/java/com/example/qualifygym_grupo13/MainActivity.kt
+++ QualifyGym_Grupo13/app/src/main/java/com/example/qualifygym_grupo13/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hola12 $name!",
+        text = "Hello My friend $name!",
         modifier = modifier
     )
 }
