736811a2e79d32570daa8c894c4f96f4fa9cd817
abraham abanto <ab.abanto@duocuc.cl>
2025-10-14 21:50:12 -0300

    Probando git

--- QualifyGym_Grupo13/app/src/main/java/com/example/qualifygym_grupo13/MainActivity.kt
+++ QualifyGym_Grupo13/app/src/main/java/com/example/qualifygym_grupo13/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Probando el commit $name!",
+        text = "Probando el commit1 $name!",
         modifier = modifier
     )
 }
