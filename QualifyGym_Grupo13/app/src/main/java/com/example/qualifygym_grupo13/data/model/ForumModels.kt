6389e3937517aef77e2b879f2bac0c8f8b099f5e
abraham abanto <ab.abanto@duocuc.cl>
2025-10-23 01:02:01 -0300

    Se crea el paquete data/model para separar los modelos de la app (para la UI/ViewModel) de las entidades de la base de datos (Room), siguiendo los principios de Arquitectura Limpia

--- QualifyGym_Grupo13/app/src/main/java/com/example/qualifygym_grupo13/data/model/ForumModels.kt
+++ QualifyGym_Grupo13/app/src/main/java/com/example/qualifygym_grupo13/data/model/ForumModels.kt
@@ -1,2 +1,20 @@
 package com.example.qualifygym_grupo13.data.model
 
+//====Nuevo ver si esta bn=====
+
+// Representa una categoría del foro
+data class Tema(
+    val id: String, // el id lo ponemos como un String ya que para el backend es mas beneficioso
+    val nombre: String,
+    val descripcion: String = "",
+    val ubicacion: String = "",
+    val numeroPublicaciones: Int = 0
+)
+
+//Representa una publicacion dentro de un tema
+data class Publicacion(
+    val id: String,
+    val titulo: String,
+    val autor: String,
+    val contenido: String
+)
