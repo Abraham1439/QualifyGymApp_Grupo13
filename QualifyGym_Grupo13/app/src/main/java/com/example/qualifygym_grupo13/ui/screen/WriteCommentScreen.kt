2d1781e22fb56d1b390dd3739a7cb6c116921867
abraham abanto <ab.abanto@duocuc.cl>
2025-10-23 16:32:09 -0300

    .Se agrego lo nuevo de la clase pa las fotos jeje

--- QualifyGym_Grupo13/app/src/main/java/com/example/qualifygym_grupo13/ui/screen/WriteCommentScreen.kt
+++ QualifyGym_Grupo13/app/src/main/java/com/example/qualifygym_grupo13/ui/screen/WriteCommentScreen.kt
@@ -1,20 +1,16 @@
 package com.example.qualifygym_grupo13.ui.screen
 
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
